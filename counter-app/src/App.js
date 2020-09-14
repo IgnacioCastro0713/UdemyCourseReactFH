be27a89e40cc06c45552874dfea7ff65070d2a79
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-09-13 21:16:51 -0500

    Initial commit

---
+++ counter-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+
+  const msg = 'this is a text in react';
+
+  return (
+    <>
+      <h1>Hello word</h1>
+      <p>{ msg }</p>
+    </>
+  );
+}
+
+export default App;
